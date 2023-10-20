42d6588c9a397ad0b3f6d9f4d77ed28de240953d
Myroslava Kukhta <107687502+myroslavaKukhta@users.noreply.github.com>
2023-10-20 10:54:10 +0300

    Styled testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,21 +4,27 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {IconWrapper} from "../skills/skill/Skill";
 
 export const Testimony = () => {
-  return (
-    <StyledTestimony>
-      <SectionTitle>Testimony</SectionTitle>
-      <FlexWrapper direction={"column"} align={"center"}>
-        <Icon iconId={"quote"}/>
-        <Slider/>
-      </FlexWrapper>
-
-    </StyledTestimony>
-  );
+    return (
+        <StyledTestimony>
+            <SectionTitle>Testimony</SectionTitle>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
+                    <Icon iconId={"quote"}/>
+                </IconWrapper>
+                <Slider/>
+            </FlexWrapper>
+        </StyledTestimony>
+    );
 };
 
 const StyledTestimony = styled.section`
   min-height: 50vh;
   background-color: yellow;
+  
+  ${IconWrapper} {
+    margin: 40px 0 72px;
+  }
 `
